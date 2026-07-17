8e14bec36b6c7336eb1b06c2653a9f328ae7adca
LeMinh-Quan <aomachx@gmail.com>
2026-07-17 08:00:17 +0700

    Resolve merge conflict

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+$viewData = [];
+$viewData["title"] = "Trang chu - Online Store";
+return view('home.index')->with("viewData", $viewData);
+});
+
 Route::get('/home', function () {
     return view('home');
 })->name('home');
