1c445542d9e3f8717aad03af2e44c76a6d239062
LeMinh-Quan <aomachx@gmail.com>
2026-07-17 07:31:57 +0700

    up file bt 3

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', $viewData["title"])
+@section('content')
+<div class="row">
+<div class="col-md-6 col-lg-4 mb-2">
+<img src="{{ asset('/img/drone.jpeg') }}" class="img-fluid rounded">
+</div>
+<div class="col-md-6 col-lg-4 mb-2">
+<img src="{{ asset('/img/game.jpeg') }}" class="img-fluid rounded"> </div>
+<div class="col-md-6 col-lg-4 mb-2">
+<img src="{{ asset('/img/philips.jpeg') }}" class="img-fluid rounded">
+</div>
+</div>
+@endsection
