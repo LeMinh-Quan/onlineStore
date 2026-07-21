fd26f5d604895bbc7b11ab3bd8b20be7dda113e3
LeMinh-Quan <aomachx@gmail.com>
2026-07-21 08:30:07 +0700

    php_codesiffer

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $viewData = [];
-    $viewData["title"] = "Trang chu - Online Store";
-    return view('home.index')->with("viewData", $viewData);
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('home.about');
 
-Route::get('/about', function () {
-
-
-    $viewData = [];
-    $viewData["title"] = "Trang chu - Online Store";
-    $viewData["subtitle"] = "About";
-    $viewData["description"] = "Day la trang ve chung toi";
-    $viewData["author"] = "yeu vo ban";
-    return view('home.about')->with("viewData", $viewData);
-})->name('home.about');
-
-
-
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
+Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
+Route::get('/products/{id}', [App\Http\Controllers\ProductController::class, 'show'])->name('product.show');
