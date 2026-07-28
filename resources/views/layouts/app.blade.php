8f2719b70d44464b04e508342385d315e3eee57b
LeMinh-Quan <aomachx@gmail.com>
2026-07-28 08:24:02 +0700

    php_28/7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,45 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Online Store')</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
-    
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" /> 
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 
-    <!-- Fixed navbar classes -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home.index') }}">OnlineStore</a> 
-            
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav ms-auto">
-                    <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
-                    <a class="nav-link active" href="{{ route('home.about') }}">About</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <header class="masthead bg-primary text-white text-center py-4">
-        <div class="container d-flex align-items-center flex-column">
-            <h2>@yield('subtitle', 'Online Store - Laravel Framework')</h2>
-        </div>
-    </header>
-
-    <div class="container my-4">
-        @yield('content')
-    </div>
-
-    <footer class="copyright py-4 text-center text-white bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4"> 
+  <div class="container"> 
+            <a class="navbar-brand" href="#">Online Store</a> 
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+aria-expanded="false" aria-label="Toggle navigation">  
+                <span class="navbar-toggler-icon"></span> 
+            </button> 
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">  
+                <div class="navbar-nav ms-auto"> 
+                    <a class="nav-link active" href="{{ route('home.index') 
+}}">Home</a> 
+                    <a class="nav-link active" href="{{ route('product.index') 
+}}">Products</a> 
+                    <a class="nav-link active" href="{{ route('home.about') 
+}}">About</a> 
+                </div> 
+            </div>  
+        </div> 
+    </nav> 
+<header class="masthead bg-primary text-white text-center py-4"> 
+<div class="container d-flex align-items-center flex-column">  
+<h2>@yield('subtitle', 'Online Store - Laravel Framework')</h2> 
+</div>  
+</header> 
+<!-- header -->
+<div class="container my-4">
+@yield('content')
+</div>
+<footer class="copyright py-4 text-center text-white bg-dark">
         <div class="container">
             <small>
                 Copyright - 
