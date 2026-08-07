a228de5730dd76eee43b97760639b112869b9058
LeMinh-Quan <aomachx@gmail.com>
2026-08-07 08:31:00 +0700

    php_7/8

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,9 +21,44 @@ class ProductController extends Controller
     $viewData = [];
     $product = Product::findOrFail($id);
     $viewData["title"] =   $product->getName()." - Online Store"; 
-$viewData["subtitle"] =   $product->getName()." - Thông tin sản phẩm."; 
+    $viewData["subtitle"] =   $product->getName()." - Thông tin sản phẩm."; 
     $viewData["product"] = $product;
     return view('product.show')->with("viewData", $viewData);
   }
+  public function create()
+    {
+        return view('product.create');
+    }
 
+    // Xử lý khi submit form
+    public function store(Request $request)
+    {
+        // Lấy dữ liệu từ form
+        $name = $request->input('product_name');
+        $price = $request->input('product_price');
+
+        // Biến chứa đường dẫn ảnh
+        $imagePath = '';
+
+        // Kiểm tra có upload ảnh hay không
+        if ($request->hasFile('product_image')) {
+            $file = $request->file('product_image');
+
+            // Lưu ảnh vào storage/app/public/products
+            $imagePath = $file->store('products', 'public');
+        }
+
+        // TODO: Lưu dữ liệu vào Database
+        // Product::create([
+        //     'name' => $name,
+        //     'price' => $price,
+        //     'image' => $imagePath,
+        // ]);
+
+        // Quay lại form và gửi thông báo
+        return redirect()
+            ->route('products.create')
+            ->with('success', "Đã thêm sản phẩm: $name với giá $price VNĐ")
+            ->with('image_path', $imagePath);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('home.about');
 
 Route::get('/products', [App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
 Route::get('/products/{id}', [App\Http\Controllers\ProductController::class, 'show'])->name('product.show');
+
+// Route hiển thị Form thêm sản phẩm (GET)
+Route::get('/product/create', [App\Http\Controllers\ProductController::class, 'create'])->name('product.create');
+// Route xử lý dữ liệu khi người dùng bấm Submit (POST)
+Route::post('/product/store', [App\Http\Controllers\ProductController::class, 'store'])->name('products.store');
